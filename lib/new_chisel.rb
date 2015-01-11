03dd2c89a76b4f12752e5dd3c2511f352072717d
Kyra Weber <kyra.m.weber@gmail.com>
2015-01-10 22:39:46 -0700

    Changed #element_checker to local variable for blocks instead of attr_reader.

--- lib/new_chisel.rb
+++ lib/new_chisel.rb
@@ -8,38 +8,32 @@ require_relative 'emphasize'
 
 class Chisel
 
-  attr_reader :element_checker
-#take a document in and immediatly seperate out chunks
-#return chunks seperated into lines to pass to parse
   def get_chunks(document)
     document.split("\n\n")
   end
 
   def parse(document)
     chunks = get_chunks(document)
-    html = []
-
-    chunks.each do |chunk|
-      @element_checker = ElementChecker.new(chunk)
+    html = chunks.map do |chunk|
+      element_checker = ElementChecker.new(chunk)
       if element_checker.paragraph?
         paragraph = Paragraph.new(chunk)
-        html << paragraph.render
+        paragraph.render
       elsif element_checker.header?
         header = Header.new(chunk)
-        html << header.render
+        header.render
       elsif element_checker.ordered_list?
         list = OrderedList.new(chunk)
-        html << list.render
+        list.render
       elsif element_checker.unordered_list?
         list = UnorderedList.new(chunk)
-        html << list.render
+        list.render
       else
         puts "Please review your markdown syntax."
       end
     end
     final_html = html.map do |chunk|
-      @element_checker = ElementChecker.new(chunk)
-      # go through each chunk again to see if strong or emph is included
+      element_checker = ElementChecker.new(chunk)
       if element_checker.strong?
         strong = Strong.new(chunk)
         strong.render
